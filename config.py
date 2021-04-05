92a5b86c558566965887a7ed9f762d2c54b5e06c
desertfox77 <68386288+desertfox77@users.noreply.github.com>
2021-04-05 15:04:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee1.chdh7mhwiyfp.us-east-1.rds.amazonaws.com"
+customhost = "employee.chdh7mhwiyfp.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "Caf3DbPassw0rd!"
-customdb = "employee1"
+customdb = "employee"
 custombucket = "addemployeeleo"
 customregion = "us-east-1"
 
